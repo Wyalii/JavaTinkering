60d70e3850242354ec9858c7ebffc3951d1ee652
Wyalii <gigaurigiorgi2727@gmail.com>
2026-07-15 17:35:52 +0400

    exploring data structures in java

---
+++ data structures/arrayList/Main.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String args[]){
+        ArrayList<String> foodList = new ArrayList<>();
+        foodList.add("Apple"); 
+        foodList.add("Bread");
+        foodList.add("Banana");
+        foodList.add("Kinhkali");
+        foodList.add("Khachapuri");
+        System.out.println(foodList);
+        // for(String food:foodList){
+        //     System.out.println(food);
+        // }
+
+        String[] newFoodList = new String[3];
+        newFoodList[0] = "Mwvadi";
+        newFoodList[1] = "Beans";
+        newFoodList[2] = "Fish";
+        // for(String food:newFoodList){
+        //     System.out.println(food);
+        // }
+
+        // for(int i = 0; i < newFoodList.length;i++){
+        //     System.out.println(newFoodList[i]);
+        // }
+        
+        System.out.println(newFoodList);
+        foodList.addAll(Arrays.asList(newFoodList));
+        System.out.println(foodList);
+        System.out.println(foodList.get(7));
+        foodList.removeAll(Arrays.asList(newFoodList));
+        System.out.println(foodList);
+        System.err.println(foodList.clone());
+        ArrayList<String> copiedFood = new ArrayList<>(foodList);
+        
+    }
+}
